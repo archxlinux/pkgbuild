9a7b22b12fdfe3ab730b4a6914a511be83daae73
Alessio <alessio@archxlinux.com>
2023-08-08 14:16:04 +0200

    remove c

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ for package_dir in */; do
     pkgrel=$(grep -m1 "^pkgrel=" PKGBUILD | cut -d= -f2 | tr -d "'")
 
     # Rebuild and install the package with makepkg -s
-    yes|makepkg -Si -s -f -C --sign --key DA6BDDD08D26A04AAC997968C79769BC07914012 
+    yes|makepkg -s -f -c -C --sign --key DA6BDDD08D26A04AAC997968C79769BC07914012 
 
     find . -maxdepth 1 -type f -name "*.tar.*" -exec mv {} /tmp/cache/ \;
 
