c463fd1ae191849d21cbcb49bcbb953c5f6e14ee
alessio <alessio@linux.com>
2025-02-13 17:16:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ for package_dir in */; do
     pkgrel=$(grep -m1 "^pkgrel=" PKGBUILD | cut -d= -f2 | tr -d "'")
 
     # Rebuild and install the package with makepkg -s
-    yes|makepkg -s -f -c -C --sign --key DA6BDDD08D26A04AAC997968C79769BC07914012 --noconfirm
+    yes|makepkg -s -f -c -C --sign --key 7CE735AE27960124DC20549D842ABE3AA3C5FCD7 --noconfirm
 
     find . -maxdepth 1 -type f -name "*.tar.*" -exec mv {} /tmp/cache/ \;
 
@@ -22,6 +22,6 @@ done
 
 cd /tmp/cache
 
-repo-add --verify --sign -k DA6BDDD08D26A04AAC997968C79769BC07914012 -n -R archx_repo.db.tar.gz *.pkg.tar.zst
+repo-add --verify --sign -k 7CE735AE27960124DC20549D842ABE3AA3C5FCD7 -n -R archx_repo.db.tar.gz *.pkg.tar.zst
 
 
